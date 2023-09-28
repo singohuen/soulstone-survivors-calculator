54319832056363e1d46accd14d94249a8fd00c3a
Singo Xuan <singohuen@gmail.com>
2023-09-28 15:04:50 +1000

    Add button, input and table

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <div>.</div>;
+  return (
+    <div>
+      <Button variant={"outline"}>Click me</Button>
+    </div>
+  );
 };
 
 export default App;
