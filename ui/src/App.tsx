65e4d62f888500791bb5becd1f71f8732ad010f5
Singo Xuan <singohuen@gmail.com>
2023-09-29 14:55:58 +1000

    Store profile in local storage

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
 import ProfileContext, { DEFAULT_PROFILE } from "./Profile.context";
 import { Profile } from "./lib/types";
 import UpgradesTable from "./UpgradesTable";
 import ProfileOverview from "./ProfileOverview";
 import { H1 } from "./components/ui/typography";
+import { useLocalStorage } from "./lib/hooks/useLocalStorage";
 
 const App = () => {
-  const [profile, setProfile] = useState<Profile>(DEFAULT_PROFILE);
+  const [profile, setProfile] = useLocalStorage<Profile>(
+    "profile",
+    DEFAULT_PROFILE
+  );
 
   return (
     <ProfileContext.Provider value={{ profile, setProfile }}>
