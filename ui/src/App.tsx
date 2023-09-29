ed1929eae1610780fbb3a3e990a120d8907a3b94
Singo Xuan <singohuen@gmail.com>
2023-09-29 14:47:32 +1000

    Add header

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,12 +3,17 @@ import ProfileContext, { DEFAULT_PROFILE } from "./Profile.context";
 import { Profile } from "./lib/types";
 import UpgradesTable from "./UpgradesTable";
 import ProfileOverview from "./ProfileOverview";
+import { H1 } from "./components/ui/typography";
 
 const App = () => {
   const [profile, setProfile] = useState<Profile>(DEFAULT_PROFILE);
 
   return (
     <ProfileContext.Provider value={{ profile, setProfile }}>
+      <header className="container pt-8">
+        <H1>Soulstone Survivors Calculator</H1>
+      </header>
+
       <main className="container pt-8">
         <ProfileOverview />
 
